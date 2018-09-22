000b591e0afe3d6e8b356a0560786772e39fd0ad
cannell001 <cannell001@gannon.edu>
2018-09-22 12:07:53 -0400

    Players array set as state

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -10,50 +10,55 @@ import TeamStats from './Team-stats';
 import Player from './Player';
 
 export default class App extends Component {
+    state = {
+        players: []
+    };
 
-    getPlayers() {
-      return [
-        {
-          _id: 1,
-          name: "Manny Henri",
-          ballManipulation: 2,
-          kickingAbilities: 3,
-          passingAbilities: 1,
-          duelTackling: 1,
-          fieldCoverage: 3,
-          blockingAbilities: 2,
-          gameStrategy: 3,
-          playmakingRisks: 2,
-        },
-        {
-          _id: 2,
-          name: "Speedy Gonz",
-          ballManipulation: 2,
-          kickingAbilities: 3,
-          passingAbilities: 1,
-          duelTackling: 1,
-          fieldCoverage: 3,
-          blockingAbilities: 2,
-          gameStrategy: 3,
-          playmakingRisks: 2,
-        },
-        {
-          _id: 3,
-          name: "Tracey Good",
-          ballManipulation: 2,
-          kickingAbilities: 3,
-          passingAbilities: 1,
-          duelTackling: 1,
-          fieldCoverage: 3,
-          blockingAbilities: 2,
-          gameStrategy: 3,
-          playmakingRisks: 2,
-        }
-      ];
+    componentWillMount(){
+        this.setState({
+            players: [
+                {
+                  _id: 1,
+                  name: "Manny Henri",
+                  ballManipulation: 2,
+                  kickingAbilities: 3,
+                  passingAbilities: 1,
+                  duelTackling: 1,
+                  fieldCoverage: 3,
+                  blockingAbilities: 2,
+                  gameStrategy: 3,
+                  playmakingRisks: 2,
+                },
+                {
+                  _id: 2,
+                  name: "Speedy Gonz",
+                  ballManipulation: 2,
+                  kickingAbilities: 3,
+                  passingAbilities: 1,
+                  duelTackling: 1,
+                  fieldCoverage: 3,
+                  blockingAbilities: 2,
+                  gameStrategy: 3,
+                  playmakingRisks: 2,
+                },
+                {
+                  _id: 3,
+                  name: "Tracey Good",
+                  ballManipulation: 2,
+                  kickingAbilities: 3,
+                  passingAbilities: 1,
+                  duelTackling: 1,
+                  fieldCoverage: 3,
+                  blockingAbilities: 2,
+                  gameStrategy: 3,
+                  playmakingRisks: 2,
+                }
+            ]
+        });
     }
 
     renderPlayers() {
-      return this.getPlayers().map((player) => (
+      return this.state.players.map((player) => (
         <TeamList key={player._id} player={player} />
       ));
     }
@@ -69,6 +74,7 @@ export default class App extends Component {
           <div className="row">
             <div className="col s12 m7" ><Player /></div>
                 <div className="col s12 m5" >
+                    <h2>Team List</h2>
                     <Divider/>
                       <List>
                         {this.renderPlayers()}
