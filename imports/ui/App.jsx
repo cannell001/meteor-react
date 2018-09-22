ee96657fbb0315adff55bcf68b35543fd02a7c89
cannell001 <cannell001@gannon.edu>
2018-09-22 11:44:03 -0400

    Map players to Team-list component using props

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
+import { List } from 'material-ui/List';
+import Divider from 'material-ui/Divider';
 
 import TeamList from './Team-list';
 import TeamStats from './Team-stats';
@@ -50,6 +52,12 @@ export default class App extends Component {
       ];
     }
 
+    renderPlayers() {
+      return this.getPlayers().map((player) => (
+        <TeamList key={player._id} player={player} />
+      ));
+    }
+
   render() {
     return (
       <MuiThemeProvider>
@@ -60,8 +68,14 @@ export default class App extends Component {
             showMenuIconButton={false}/>
           <div className="row">
             <div className="col s12 m7" ><Player /></div>
+                <div className="col s12 m5" >
+                    <Divider/>
+                      <List>
+                        {this.renderPlayers()}
+                      </List>
+                    <Divider/>
+                </div>
             <div className="col s12 m5" ><TeamStats /></div>
-            <div className="col s12 m5" ><TeamList /></div>
           </div>
         </div>
       </MuiThemeProvider>
