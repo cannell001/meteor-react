3a88885224cb41946740bfd63e4bcbd7dac6a8a6
cannell001 <cannell001@gannon.edu>
2018-09-22 10:19:10 -0400

    Chip Components

--- imports/ui/Player.jsx
+++ imports/ui/Player.jsx
@@ -1,5 +1,22 @@
 import React, { Component } from 'react';
 import { Card, CardMedia, CardTitle, CardText, CardActions } from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import Avatar from 'material-ui/Avatar';
+import Chip from 'material-ui/Chip';
+import { blue200, blue900 } from 'material-ui/styles/colors';
+
+const styles = {
+  chip: {
+    margin: 4,
+  },
+  wrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  button: {
+    margin: 12,
+  },
+};
 
 export default class Player extends Component {
   render() {
@@ -11,10 +28,87 @@ export default class Player extends Component {
           <img src="player.jpg" />
         </CardMedia>
         <CardText>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          <Chip
+          backgroundColor={blue200}
+          style={styles.chip}
+          >
+            <Avatar size={32} color={blue200} backgroundColor={blue900}>
+              0
+            </Avatar>
+            Ball manipulation
+          </Chip>
+          <Chip
+          backgroundColor={blue200}
+          style={styles.chip}
+          >
+          <Avatar size={32} color={blue200} backgroundColor={blue900}>
+            0
+          </Avatar>
+          Kicking abilities
+        </Chip>
+        <Chip
+          backgroundColor={blue200}
+          style={styles.chip}
+          >
+          <Avatar size={32} color={blue200} backgroundColor={blue900}>
+            0
+          </Avatar>
+          Passing abilities
+        </Chip>
+        <Chip
+          backgroundColor={blue200}
+          style={styles.chip}
+          >
+          <Avatar size={32} color={blue200} backgroundColor={blue900}>
+            0
+          </Avatar>
+          Duel/Tackling abilities
+        </Chip>
+        <Chip
+          backgroundColor={blue200}
+          style={styles.chip}
+          >
+          <Avatar size={32} color={blue200} backgroundColor={blue900}>
+            0
+          </Avatar>
+          Field speed coverage
+        </Chip>
+        <Chip
+          backgroundColor={blue200}
+          style={styles.chip}
+          >
+          <Avatar size={32} color={blue200} backgroundColor={blue900}>
+            0
+          </Avatar>
+          Blocking abilities
+        </Chip>
+        <Chip
+          backgroundColor={blue200}
+          style={styles.chip}
+          >
+          <Avatar size={32} color={blue200} backgroundColor={blue900}>
+            0
+          </Avatar>
+          Ball Manipulation
+        </Chip>
+        <Chip
+          backgroundColor={blue200}
+          style={styles.chip}
+          >
+          <Avatar size={32} color={blue200} backgroundColor={blue900}>
+            0
+          </Avatar>
+          Game strategy
+        </Chip>
+        <Chip
+          backgroundColor={blue200}
+          style={styles.chip}
+          >
+          <Avatar size={32} color={blue200} backgroundColor={blue900}>
+            0
+          </Avatar>
+          Playmaking risks
+        </Chip>
         </CardText>
         <CardActions>
 
