47fbe098b2c26c1da1086115fa2f11328f541a3b
cannell001 <cannell001@gannon.edu>
2018-09-22 12:37:57 -0400

    Test routes

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import App from '../imports/ui/App.jsx';
+import Example from '../imports/ui/Example';
+import Lost from '../imports/ui/Lost';
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('render-target'));
+  render((
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App}/>
+        <Route path="/example" component={Example}/>
+        <Route component={Lost}/>
+      </Switch>
+    </Router>
+  ), document.getElementById('render-target'));
 });
